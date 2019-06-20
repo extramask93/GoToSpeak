3ee46fbd68acf67a09ca3ac043b91aa640f6e461
Damian Jozwiak <damian.jozwiak@synapse.com>
2019-06-20 18:31:13 +0200

    update

--- GoToSpeak-SPA/src/app/_resolvers/room.resolver.ts
+++ GoToSpeak-SPA/src/app/_resolvers/room.resolver.ts
@@ -21,7 +21,6 @@ export class RoomResolver implements Resolve<Room[]> {
 
     resolve(route: ActivatedRouteSnapshot): Observable<Room[]> {
         if (this.signalrService.connectionIsEstablished) {
-
         return from(this.signalrService.getRooms());
         }
         return of(null);
